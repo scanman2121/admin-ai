d29f81f711d67b1e6e3ee528d2ec034331a190a7
Christopher Scanlon <christopher.scanlon@hqo.co>
2025-11-24 13:09:23 -0500

    Increase spacing between feature items and pricing card from gap-8 to gap-12

--- src/app/(main)/feature-gate/page.tsx
+++ src/app/(main)/feature-gate/page.tsx
@@ -52,7 +52,7 @@ export default function FeatureGatePage() {
         </div>
 
         {/* Main Content Grid */}
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-12">
           {/* Left Column - Features */}
           <div className="lg:col-span-2 space-y-8">
             {feature.features.map((featureItem, index) => {
